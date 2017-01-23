280208b641ede4654e4e080e23f4b61e5bf2eaaa
Erik Killops and Kevin Finley <erik@>
2017-01-23 09:30:20 -0800

    add dynamic routing for albums

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Album } from '../album.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-marketplace',
@@ -8,6 +9,9 @@ import { Album } from '../album.model';
 })
 
 export class MarketplaceComponent {
+
+  constructor(private router: Router){}
+
   albums: Album[] = [
     new Album("Pulse", "Pink Floyd",
        "A live  album by the English progressive rock band originally released in 1995, on the label EMI in the United Kingdom.", 1),
@@ -22,4 +26,9 @@ export class MarketplaceComponent {
    new Album("Axis Bold As Love", "The Jimi Hendrix Experience",
        "Second studio album by the English-American band, released in 1967.", 6)
  ];
+
+ goToDetailPage(clickedAlbum: Album): void {
+   this.router.navigate(['albums', clickedAlbum.id])
+ }
+
 }
